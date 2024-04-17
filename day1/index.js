613264d553c01a08bf044f8e74f7b3d29a9bd81e
bashartech <144168218+bashartech@users.noreply.github.com>
2024-04-18 01:10:21 +0500

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,13 @@
+// Question 1 completed
+
+// Question 2
+
+var nam = "Bashar";
+console.log("Hello ".concat(nam, " Would you like to learn some Typescript Today?"));
+
+// Question 3
+
+var nam1 = "Bashar";
+console.log(nam1.toLowerCase());
+console.log(nam1.toUpperCase());
+console.log(nam1.charAt[0].toUpperCase() + nam1.slice[1].toLowerCase);
---
+++ day1/index.ts
@@ -0,0 +1,15 @@
+// DAY 1
+
+// Question 1 completed
+
+// Question 2
+
+let nam:string ="Bashar";
+console.log(`Hello ${nam} Would you like to learn some Typescript Today?`) 
+
+// Question 3
+
+let nam1 : string = "Bashar"
+console.log(nam1.toLowerCase())
+console.log(nam1.toUpperCase())
+console.log(nam1.charAt[0].toUpperCase()+nam1.slice[1].toLowerCase)
